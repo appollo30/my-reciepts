94bb4365f8067d688d0dee0cc717ebd17dee50c8
appollo30 <leo.lasnier@me.com>
2025-01-27 18:25:10 +0100

    COmpletely switched the ocr dependency, switched easyocr for paddleocr

--- src/reader.py
+++ src/reader.py
@@ -1,2 +1,9 @@
+import os
 from PIL import Image
+import numpy as np
 
+def read_image(file_name : str) -> Image:
+    path = os.path.join("data",file_name)
+    
+    im = Image.open(path).convert("RGB")
+    return im
